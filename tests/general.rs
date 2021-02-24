8fda12c2a3e2ee4198959eee10e88794a13ca2ba
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-02-23 20:51:07 -0500

    get to a better error
    it compiles!

--- tests/general.rs
+++ tests/general.rs
@@ -1,8 +1,8 @@
+use near_sdk::{json_types::ValidAccountId, serde_json::json, PendingContractTx};
 use near_sdk_sim::{
-    call, deploy, init_simulator, near_crypto::Signer, to_yocto, view, ContractAccount,
-    UserAccount, STORAGE_AMOUNT,
+    deploy, init_simulator, near_crypto::Signer, to_yocto, ContractAccount, UserAccount,
+    DEFAULT_GAS, STORAGE_AMOUNT,
 };
-use near_sdk::json_types::ValidAccountId;
 
 /// Bring contract crate into namespace
 extern crate fungible_token;
@@ -17,9 +17,7 @@ near_sdk_sim::lazy_static! {
     static ref TOKEN_WASM_BYTES: &'static [u8] = include_bytes!("../res/fungible_token.wasm").as_ref();
 }
 
-fn init(
-    initial_balance: u128,
-) -> (UserAccount, ContractAccount<ContractContract>, UserAccount) {
+fn init(initial_balance: u128) -> (UserAccount, ContractAccount<ContractContract>, UserAccount) {
     let master_account = init_simulator(None);
     // println!("aloha {}", master_account.account_id());
     // uses default values for deposit and gas
@@ -37,7 +35,16 @@ fn init(
         (owner_id: ValidAccountId, total_supply: U128, version: String, name: String, symbol: String, reference: String, reference_hash: String, decimals: u8)
          */
 
-        init_method: new(ValidAccountId::try_from(master_account.account_id()).unwrap(), initial_balance.into(), "1.0.0".to_string(), "Mochi Tokens".to_string(), "MOCHI".to_string(),"https://example.com/mochi.json".to_string(), "7c879fa7b49901d0ecc6ff5d64d7f673da5e4a5eb52a8d50a214175760d8919a".to_string(), 19)
+        init_method: new(
+            ValidAccountId::try_from(master_account.account_id()).unwrap(),
+            initial_balance.into(),
+            "1.0.0".to_string(),
+            "Mochi Tokens".to_string(),
+            "MOCHI".to_string(),
+            "https://example.com/mochi.json".to_string(),
+            "7c879fa7b49901d0ecc6ff5d64d7f673da5e4a5eb52a8d50a214175760d8919a".to_string(),
+            19
+        )
     );
     let alice = master_account.create_user("alice".to_string(), to_yocto("100"));
     (master_account, contract_user, alice)
@@ -68,15 +75,30 @@ fn test_sim_transfer() {
     let initial_balance = to_yocto("100000");
     let (master_account, contract, alice) = init(initial_balance);
     // Uses default gas amount, `near_sdk_sim::DEFAULT_GAS`
-    let res = call!(
-        master_account,
-        contract.ft_transfer(alice.account_id(), transfer_amount.into()),
-        deposit = STORAGE_AMOUNT
+    let res = master_account.call(
+        PendingContractTx::new(
+            &contract.user_account.account_id(),
+            "ft_transfer",
+            json!({ "account_id": alice.account_id(), "transfer_amount": transfer_amount }),
+            false,
+        ),
+        STORAGE_AMOUNT,
+        DEFAULT_GAS,
     );
     println!("{:#?}\n Cost:\n{:#?}", res.status(), res.profile_data());
     assert!(res.is_ok());
 
-    let value = view!(contract.ft_balance_of(master_account.account_id()));
+    let value = master_account.call(
+        PendingContractTx::new(
+            "contract",
+            "ft_balance_of",
+            json!({ "account_id": alice.account_id() }),
+            true,
+        ),
+        STORAGE_AMOUNT,
+        DEFAULT_GAS,
+    );
+    // let value = view!(contract.ft_balance_of(master_account.account_id()));
     let value: U128 = value.unwrap_json();
     assert_eq!(initial_balance - transfer_amount, value.0);
 }
